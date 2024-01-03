dcfdc255e3bf3366716bd420e0206f443a6146d6
shyam286 <ghanshyamgurnule05@gmail.com>
2024-01-04 00:55:26 +0530

    first commit

---
+++ FPDemo/my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Operation from './Operation';
+function App() {
+  return (
+    <div className="App">
+        <Operation/>
+    </div>
+  );
+}
+
+export default App;
---
+++ FPDemo/my-app/src/Operation.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+
+import FriendForm from './FriendForm';
+import FriendList from './FriendList';
+
+const AppOP = () => {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    const storedFriends = JSON.parse(localStorage.getItem('friends'));
+    if (storedFriends) {
+      setFriends(storedFriends);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('friends', JSON.stringify(friends));
+  }, [friends]);
+
+  const handleAddFriend = (newFriend) => {
+    setFriends([...friends, newFriend]);
+  };
+
+  const handleEditFriend = (friendToUpdate) => {
+    const updatedFriends = friends.map((friend) =>
+      friend.id === friendToUpdate.id ? friendToUpdate : friend
+    );
+    setFriends(updatedFriends);
+  };
+
+  const handleDeleteFriend = (id) => {
+    const updatedFriends = friends.filter((friend) => friend.id !== id);
+    setFriends(updatedFriends);
+  };
+
+  return (
+    <div className="App">
+      <h1>Friend Notebook</h1>
+      <FriendForm onAddFriend={handleAddFriend} />
+      <FriendList
+        friends={friends}
+        onEditFriend={handleEditFriend}
+        onDeleteFriend={handleDeleteFriend}
+      />
+    </div>
+  );
+};
+
+export default AppOP;
